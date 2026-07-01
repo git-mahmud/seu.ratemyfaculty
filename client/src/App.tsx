6e4ab6716c0d7381d51ed311c7f6d47110737bc0
git-mahmood <2025100000379@seu.edu.bd>
2026-07-01 22:42:08 +0600

    Implement ScrollToTop component in Router
    Added ScrollToTop component to reset scroll position on route change.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,19 +15,30 @@ import PrivacyPolicy from "@/pages/PrivacyPolicy";
 import TermsOfService from "@/pages/TermsOfService";
 import NotFound from "@/pages/not-found";
 
+function ScrollToTop() {
+  const [location] = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null;
+}
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/teacher/:id" component={TeacherProfile} />
-      <Route path="/auth" component={AuthPage} />
-      <Route path="/admin" component={AdminDashboard} />
-      <Route path="/favorites" component={Favorites} />
-      <Route path="/about" component={About} />
-      <Route path="/privacy" component={PrivacyPolicy} />
-      <Route path="/terms" component={TermsOfService} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <ScrollToTop />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/teacher/:id" component={TeacherProfile} />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/admin" component={AdminDashboard} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/about" component={About} />
+        <Route path="/privacy" component={PrivacyPolicy} />
+        <Route path="/terms" component={TermsOfService} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
